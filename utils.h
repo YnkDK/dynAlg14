8fa1a991cb493788e7f23ef9a70fe2e08cd88de9
Martin Storgaard <20140000@post.au.dk>
2015-05-08 17:01:36 +0200

    Added random number generator

--- utils.h
+++ utils.h
@@ -7,16 +7,27 @@
 **
 ** Author: Martin Storgaard and Konstantinos Mampentzidis
 ** -------------------------------------------------------------------------*/
-#include <iostream> //< TODO: Remove this and main function
-#include <cstdlib> //< div
 #include <stdint.h> //< uint32_t uint64_t
+#include <random>	//< Mersenne twister random number engine
+
+#ifndef P
+	#define P 2147483647ULL	//!< 2^31 - 1 = 2147483647
+#endif
+#ifndef SEED
+	#define SEED 42
+#endif
+
+static std::mt19937 mersenne_twister_ (SEED);
+static std::uniform_int_distribution<uint32_t> distribution_(0, P - 1);
+
+inline uint32_t next() {
+	return distribution_(mersenne_twister_);
+}
 
 /**
  * It is assumed that a and b is strictly smaller than P in all functions below
  */
  
-#define P 2147483647ULL	//!< 2^31 - 1 = 2147483647
-
 inline uint32_t field(const uint32_t a) {
 	return (a >= P) ? a % P : a;
 }
@@ -36,20 +47,25 @@ inline uint32_t mod_mul(const uint32_t a, const uint32_t b) {
 	return (res >= P) ? res % P : res;
 }
 
-inline uint32_t mod_inv(const uint32_t a, const uint32_t p) {
-
-}
-
-
-
-int main() {
-//	std::cout << mod_add(2147483645UL, 1231) << std::endl;
-//	std::cout << (2147483645ULL+1231) % P << std::endl;
+/**
+ *	Inspired by http://rosettacode.org/wiki/Modular_inverse#C.2B.2B
+ *	TODO: Might be optimized
+ */
+inline uint32_t mod_inv(uint32_t a) {
+	int32_t b = P;
 	
-//	std::cout << mod_sub(1231, 2147483645UL) << std::endl;
-//	std::cout << ((1231-2147483645UL)+P) % P << std::endl;
-	
-//	std::cout << mod_mul(2147483645UL, 2147483645UL) << std::endl;
-//	std::cout << (2147483645UL * 2147483645UL) % P << std::endl;
-	std::cout << mod_inv(123125) << " < " << P << std::endl;
+	int32_t t, q;
+	int32_t x0 = 0, x1 = 1;
+	while (a > 1) {
+		q = a / b;
+		
+		t = b;
+		b = a % b;
+		a = t;
+		t = x0;
+		x0 = x1 - q * x0;
+		x1 = t;
+	}
+	if (x1 < 0) x1 += P;
+	return x1;
 }
