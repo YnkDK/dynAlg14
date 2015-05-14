b00d148561f1d8337725eae30b4476f7f29114b0
kmampent <kmampent@cs.au.dk>
2015-05-14 18:13:10 +0200

    attempt to fix delete function

--- TD_EAGER.cpp
+++ TD_EAGER.cpp
@@ -82,9 +82,55 @@ void TD_EAGER::updateInverseMatrix(int i, int j, uint32_t u)
 
 
 void TD_EAGER::ins(int i, int j) {
-	uint32_t v = next();
-	adjacency_matrix[i*n + j] = v;
-	updateInverseMatrix(i,j, v);
+	uint32_t u = next();
+	adjacency_matrix[i*n + j] = u;
+	
+	//update the inverse matrix
+	
+	uint32_t v = 1;
+	int k,m;
+	//find (A^(-1)*u) => pick i-th column of A^(-1) and multiply every value by u
+	uint32_t *a = new uint32_t[n];
+	for(k=0;k<n;k++)
+		a[k] = mod_mul(inverse_matrix[k*n+i],u);
+	
+	//find (v^T*A^(-1)) => pick j-th row of A^(-1) and multiply every value by v
+	uint32_t *b = new uint32_t[n];
+	for(k=0;k<n;k++)
+		b[k] = mod_mul(inverse_matrix[j*n+k],v);
+	
+	//find v^T*A^(-1)*u
+	uint32_t c = 0;
+	for(k=0;k<n;k++)
+		c = mod_add(c, mod_mul(a[k],b[k]));
+	
+	//find the denominator of sherman morisson formula
+	uint32_t d = mod_add(c,1);
+	
+	if(d == 0){
+		std::cout<<"Division by zero!!!!!!!!"<<std::endl;
+		exit(1);
+	}
+	
+	//find the nominator of sherman morisson formula and divide with the denominator at the same time.
+	uint32_t *e = new uint32_t[n*n];
+	for(k=0;k<n;k++)
+		for(m=0;m<n;m++)
+			e[k*n+m] = mod_mul(mod_inv(d),mod_mul(a[k],b[m]));
+	
+	//update the inverse matrix and counter
+	count = 0;
+	for(k=0;k<n;k++){
+		for(m=0;m<n;m++){
+			int index = k*n+m;
+			inverse_matrix[index] = mod_sub(inverse_matrix[index], e[index]);
+			if(inverse_matrix[index]!=0) count++;
+		}
+	}
+		
+	delete[] a;
+	delete[] b;
+	delete[] e;
 	/*cout << "INSERT" << endl;
 	printMatrix(adjacency_matrix, n);
 	printMatrix(inverse_matrix, n);
@@ -92,12 +138,61 @@ void TD_EAGER::ins(int i, int j) {
 }
 
 void TD_EAGER::del(int i, int j) {
-	updateInverseMatrix(i, j, (uint32_t) (P - adjacency_matrix[i*n + j]));
+	//update inverse matrix
+	
+	uint32_t u = adjacency_matrix[i*n + j];
 	adjacency_matrix[i*n + j] = 0;
-    cout << "DELETE" << endl;
+
+	//update the inverse matrix
+	
+	uint32_t v = 1;
+	int k,m;
+	//find (A^(-1)*u) => pick i-th column of A^(-1) and multiply every value by u
+	uint32_t *a = new uint32_t[n];
+	for(k=0;k<n;k++)
+		a[k] = mod_mul(inverse_matrix[k*n+i],u);
+	
+	//find (v^T*A^(-1)) => pick j-th row of A^(-1) and multiply every value by v
+	uint32_t *b = new uint32_t[n];
+	for(k=0;k<n;k++)
+		b[k] = mod_mul(inverse_matrix[j*n+k],v);
+	
+	//find v^T*A^(-1)*u
+	uint32_t c = 0;
+	for(k=0;k<n;k++)
+		c = mod_add(c, mod_mul(a[k],b[k]));
+	
+	//find the denominator of sherman morisson formula
+	uint32_t d = mod_sub(1,c);
+	
+	if(d == 0){
+		std::cout<<"Division by zero!!!!!!!!"<<std::endl;
+		exit(1);
+	}
+	
+	//find the nominator of sherman morisson formula and divide with the denominator at the same time.
+	uint32_t *e = new uint32_t[n*n];
+	for(k=0;k<n;k++)
+		for(m=0;m<n;m++)
+			e[k*n+m] = mod_mul(mod_inv(d),mod_mul(a[k],b[m]));
+	
+	//update the inverse matrix and counter
+	count = 0;
+	for(k=0;k<n;k++){
+		for(m=0;m<n;m++){
+			int index = k*n+m;
+			inverse_matrix[index] = mod_add(inverse_matrix[index], e[index]);
+			if(inverse_matrix[index]!=0) count++;
+		}
+	}
+		
+	delete[] a;
+	delete[] b;
+	delete[] e;
+    /*cout << "DELETE" << endl;
     printMatrix(adjacency_matrix, n);
     printMatrix(inverse_matrix, n);
-    cout << endl << endl;
+    cout << endl << endl;*/
 }
 
 unsigned int TD_EAGER::query() {
